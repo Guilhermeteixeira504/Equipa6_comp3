e3c5f50783d9d03aa22ef8e564bc3ff36a4c95f9
Bruninha9 <brunajasmin15@gmail.com>
2025-12-12 19:48:40 +0000

    DTO da inscricao

---
+++ InscricaoDTO.java
@@ -0,0 +1,30 @@
+package lp.JavaFxClient.model;
+
+public class InscricaoDTO {
+
+    private Long id;
+    private Long programaId;
+    private Long voluntarioId;
+    private int nHorasRealizadas;
+
+    public InscricaoDTO() {}
+
+    public InscricaoDTO(Long id, Long programaId, Long voluntarioId, int nHorasRealizadas) {
+        this.id = id;
+        this.programaId = programaId;
+        this.voluntarioId = voluntarioId;
+        this.nHorasRealizadas = nHorasRealizadas;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public Long getProgramaId() { return programaId; }
+    public void setProgramaId(Long programaId) { this.programaId = programaId; }
+
+    public Long getVoluntarioId() { return voluntarioId; }
+    public void setVoluntarioId(Long voluntarioId) { this.voluntarioId = voluntarioId; }
+
+    public int getnHorasRealizadas() { return nHorasRealizadas; }
+    public void setnHorasRealizadas(int nHorasRealizadas) { this.nHorasRealizadas = nHorasRealizadas; }
+}
