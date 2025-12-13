2c89c1c425d925389e1b1bb8a94c94d48c3554ac
carolina15afonso-dot <carolina15afonso@gmail.com>
2025-12-13 14:21:01 +0000

    Controller do menu admin

---
+++ MenuAdminController.java
@@ -0,0 +1,34 @@
+package lp.JavaFxClient.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
+public class MenuAdminController {
+	
+	@FXML
+	private MenuItem itemCriarTipo;
+	@FXML
+	private MenuItem itemRegistarInstituicao;
+	@FXML
+	private MenuItem itemCriarPrograma;
+	@FXML
+	private MenuItem itemCriarEdicao;
+	@FXML
+	private MenuItem itemListarProgramas;
+	@FXML
+	private MenuItem itemProcurarPrograma;
+	@FXML
+	private MenuItem itemListarUtilizadores;
+	@FXML
+	private MenuItem itemListarVoluntarios;
+	@FXML
+	private MenuItem itemListarInscricoes;
+	@FXML
+	private MenuItem itemDefinirHorasTrabalhadas;
+	@FXML
+	private MenuItem itemListarInstituicoes;
+	
+	
+	
+
+}
