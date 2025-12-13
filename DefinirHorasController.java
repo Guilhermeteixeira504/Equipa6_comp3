09df302ef3aa40456c22d128d52179b6b77a498a
Bruno <50420@alunos.upt.pt>
2025-12-13 21:17:51 +0000

    Controller do método ( Definir Hora )

---
+++ DefinirHorasController.java
@@ -0,0 +1,54 @@
+package lp.JavaFxClient.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import lp.JavaFxClient.services.ApiService;
+
+public class DefinirHorasController {
+
+    @FXML
+    private TextField inscricaoIdField;
+
+    @FXML
+    private TextField horasField;
+
+    @FXML
+    private TextArea resultadoArea;
+
+    private final ApiService api = new ApiService();
+
+    @FXML
+    private void registarHoras() {
+        try {
+            long inscricaoId = Long.parseLong(inscricaoIdField.getText());
+            int horas = Integer.parseInt(horasField.getText());
+
+            String json = """
+                {
+                    "nHorasRealizadas": %d
+                }
+            """.formatted(horas);
+
+            String response = api.put(
+                "/voluntariado/inscricoes/" + inscricaoId + "/horas",
+                json
+            );
+
+            if (response.startsWith("ERROR")) {
+                resultadoArea.setText(response);
+            } else {
+                resultadoArea.setText("Horas registadas com sucesso!");
+            }
+
+        } catch (NumberFormatException e) {
+            resultadoArea.setText("Erro: valores inválidos.");
+        }
+    }
+
+    private void mostrarErro(String msg) {
+        resultadoArea.setText(msg);
+        new Alert(Alert.AlertType.ERROR, msg).showAndWait();
+    }
+}
