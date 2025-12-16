9c0ae5e250dd93ed1644a85b4bd0dc5e9b6be71a
AndreSousa <andre2005rs@gmail.com>
2025-12-16 18:23:33 +0000

    controller do pocurarporId

--- ProcurarPorIdController.java
+++ ProcurarPorIdController.java
@@ -1,29 +1,46 @@
 package lp.JavaFxClient.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import lp.JavaFxClient.DTO.ProgramaVoluntariadoDTO;
 import lp.JavaFxClient.services.ApiService;
 
 public class ProcurarPorIdController {
 private final ApiService api = new ApiService();
+private final ObjectMapper mapper = new ObjectMapper();
 	
 	@FXML
 	private TextField txtPrograma;
 	@FXML
-	private Button buttonCriar;
+	private Button buttonProcurar;
 	@FXML
 	private TextArea resultadoArea;
 	
-	@FXML
-	private void onProcurarPrograma(ActionEvent event) {
-		try {
-			//for(ProgramaVoluntariado programa : programas)
-		} catch (Exception e) {
-			new Alert(Alert.AlertType.ERROR, "Erro: " + e.getMessage()).showAndWait();
-			}
-	}
+	  private void onProcurarPrograma(ActionEvent event) {
+	        try {
+
+	            String idText = txtPrograma.getText();
+	            int id = Integer.parseInt(idText);
+	            
+	            String endpoint = "/voluntariado/programasvoluntariado/" + id;
+	            String json = api.get(endpoint);
+	            
+	            if (json.startsWith("ERROR:")) {
+	                resultadoArea.setText("Programa com ID " + id + " não encontrado.");
+	                return;
+	            }
+	            
+	            ProgramaVoluntariadoDTO programa = mapper.readValue(json, ProgramaVoluntariadoDTO.class);
+	            
+	            resultadoArea.setText(programa.toString());
+	            
+	        } catch (Exception e) {
+	            resultadoArea.setText("Erro: " + e.getMessage());
+	        }
+	    }
 }
