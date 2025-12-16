0d5be69f06bd2397c80cdb3afb0df46fd0bbb16d
Bruninha9 <brunajasmin15@gmail.com>
2025-12-16 21:09:01 +0000

    controller listar instituicoes atualizado

--- ListarInstituicoesController.java
+++ ListarInstituicoesController.java
@@ -14,40 +14,48 @@ import lp.JavaFxClient.DTO.InstituicaoDTO;
 import lp.JavaFxClient.services.ApiService;
 
 public class ListarInstituicoesController {
-	private final ApiService api = new ApiService();
-	private final ObjectMapper mapper = new ObjectMapper();
-	
-	@FXML
-	private TableView <InstituicaoDTO> instituicoesTable;
-	@FXML
-	private TableColumn <InstituicaoDTO,String>colunaNome;
-	@FXML
-	private TableColumn <InstituicaoDTO,Long>colunaPrograma;
-	
-	public void initialize() {
+
+    private final ApiService api = new ApiService();
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @FXML
+    private TableView<InstituicaoDTO> instituicoesTable;
+
+    @FXML
+    private TableColumn<InstituicaoDTO, String> colunaNome;
+
+    @FXML
+    private TableColumn<InstituicaoDTO, String> colunaProgramas;
+
+    @FXML
+    public void initialize() {
         colunaNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
-        colunaPrograma.setCellValueFactory(new PropertyValueFactory<>("programa"));
+        colunaProgramas.setCellValueFactory(new PropertyValueFactory<>("programasAsString"));
 
         loadInstituicoes();
     }
-	private void loadInstituicoes() {
-		try {
-			String json = api.get("/voluntariado/instituicoes");
-			if (json.startsWith("ERROR:")) {
-					showError(json);
-					return;
-				 }
-			List<InstituicaoDTO> instituicoes =
-						mapper.readValue(json, new TypeReference<List<InstituicaoDTO>>() {});
-					 instituicoesTable.getItems().setAll(instituicoes);
-		}catch (Exception e) {
-		 showError("Erro a Carregar Instituições: " + e.getMessage());
-		}
-}
-	
-	private void showError(String msg) {
-		Alert a = new Alert(Alert.AlertType.ERROR, msg);
-		a.showAndWait();
-		}
-	
+
+    private void loadInstituicoes() {
+        try {
+            String json = api.get("/voluntariado/instituicoes");
+
+            if (json.startsWith("ERROR:")) {
+                showError(json);
+                return;
+            }
+
+            List<InstituicaoDTO> instituicoes =
+                    mapper.readValue(json, new TypeReference<List<InstituicaoDTO>>() {});
+
+            instituicoesTable.getItems().setAll(instituicoes);
+
+        } catch (Exception e) {
+            showError("Erro a carregar instituições: " + e.getMessage());
+        }
+    }
+
+    private void showError(String msg) {
+        Alert a = new Alert(Alert.AlertType.ERROR, msg);
+        a.showAndWait();
+    }
 }
